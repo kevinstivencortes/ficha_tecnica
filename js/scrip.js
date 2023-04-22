6d23d1cecf8f43287d73b1e04572fa9811625753
kevinstivencortes <kevinstivencortesmaigual36@gmail.com>
2023-04-21 20:30:06 -0500

    add todo

---
+++ js/scrip.js
@@ -0,0 +1,142 @@
+function comprobar() {
+    let producto, linea, modulo, descripcion, arquitectura, requisitos, hardware, software, otros, funcionales, adicionales,clientes
+    producto=document.getElementById ("producto").value
+    document.getElementById("resultado1").innerHTML="<h3> nombre del producto: "+producto+"</h3>"
+    console.log(producto)
+
+    linea=document.getElementById ("linea").value
+    document.getElementById("resultado2").innerHTML="<h3> linea del producto: "+linea+"</h3>"
+    console.log(linea)
+
+    modulo=document.getElementById ("modulo").value
+    document.getElementById("resultado3").innerHTML="<h3> modulo: "+modulo+"</h3>"
+    console.log(modulo)
+
+    descripcion=document.getElementById ("descripcion").value
+    document.getElementById("resultado4").innerHTML="<h3> descripcion: "+descripcion+"</h3>"
+    console.log(descripcion)
+
+    arquitectura=document.getElementById ("arquitectura").value
+    document.getElementById("resultado5").innerHTML="<h3> arquitectura: "+arquitectura+"</h3>"
+    console.log(arquitectura)
+
+    requisitos=document.getElementById ("requisitos").value
+    document.getElementById("resultado6").innerHTML="<h3> requisitos (servidor): "+requisitos+"</h3>"
+    console.log(requisitos)
+
+    hardware=document.getElementById ("hardware").value
+    document.getElementById("resultado7").innerHTML="<h3> hardware (servidor): "+hardware+"</h3>"
+    console.log(hardware)
+
+    software=document.getElementById ("software").value
+    document.getElementById("resultado8").innerHTML="<h3> software (servidor): "+software+"</h3>"
+    console.log(software)
+
+    otros=document.getElementById ("otros").value
+    document.getElementById("resultado9").innerHTML="<h3> otros (servidor): "+otros+"</h3>"
+    console.log(otros)
+
+    requisitos1=document.getElementById ("requisitos1").value
+    document.getElementById("resultado10").innerHTML="<h3> requisitos (cliente): "+requisitos1+"</h3>"
+    console.log(requisitos1)
+
+    hardware1=document.getElementById ("hardware1").value
+    document.getElementById("resultado11").innerHTML="<h3> hardware (cliente): "+hardware1+"</h3>"
+    console.log(hardware1)
+
+    software1=document.getElementById ("software1").value
+    document.getElementById("resultado12").innerHTML="<h3> software (cliente): "+software1+"</h3>"
+    console.log(software1)
+
+    otros1=document.getElementById ("otros1").value
+    document.getElementById("resultado13").innerHTML="<h3> otros (cliente): "+otros1+"</h3>"
+    console.log(otros1)
+
+    funcionales=document.getElementById ("funcionales").value
+    document.getElementById("resultado14").innerHTML="<h3> funcionales generales: "+funcionales+"</h3>"
+    console.log(funcionales)
+
+    adicionales=document.getElementById ("adicionales").value
+    document.getElementById("resultado15").innerHTML="<h3> requerimientos adicionales: "+adicionales+"</h3>"
+    console.log(adicionales)
+
+    clientes=document.getElementById ("clientes").value
+    document.getElementById("resultado16").innerHTML="<h3> clientes del productos: "+clientes+"</h3>"
+    console.log(clientes)
+
+    version=document.getElementById ("version").value
+    document.getElementById("resultado17").innerHTML="<h3> version: "+version+"</h3>"
+    console.log(version)
+
+    naturaleza=document.getElementById ("naturaleza").value
+    document.getElementById("resultado18").innerHTML="<h3> naturaleza: "+naturaleza+"</h3>"
+    console.log(naturaleza)
+
+    aprovacion=document.getElementById ("aprovacion").value
+    document.getElementById("resultado19").innerHTML="<h3> aprovacion: "+aprovacion+"</h3>"
+    console.log(aprovacion)
+
+    validacion=document.getElementById ("validacion").value
+    document.getElementById("resultado20").innerHTML="<h3> validacion: "+validacion+"</h3>"
+    console.log(validacion)
+
+    version1=document.getElementById ("version1").value
+    document.getElementById("resultado21").innerHTML="<h3> version: "+version1+"</h3>"
+    console.log(version1)
+
+    naturaleza1=document.getElementById ("naturaleza1").value
+    document.getElementById("resultado22").innerHTML="<h3> naturaleza: "+naturaleza1+"</h3>"
+    console.log(naturaleza1)
+
+    aprovacion1=document.getElementById ("aprovacion1").value
+    document.getElementById("resultado23").innerHTML="<h3> aprovacion: "+aprovacion1+"</h3>"
+    console.log(aprovacion1)
+
+    validacion1=document.getElementById ("validacion1").value
+    document.getElementById("resultado24").innerHTML="<h3> validacion: "+validacion1+"</h3>"
+    console.log(validacion1)
+
+    elaboro=document.getElementById ("elaboro").value
+    document.getElementById("resultado25").innerHTML="<h3> elaboro: "+elaboro+"</h3>"
+    console.log(elaboro)
+
+    aprobo=document.getElementById ("aprobo").value
+    document.getElementById("resultado26").innerHTML="<h3> aprobo: "+aprobo+"</h3>"
+    console.log(aprobo)
+
+    valido=document.getElementById ("valido").value
+    document.getElementById("resultado27").innerHTML="<h3> valido: "+valido+"</h3>"
+    console.log(valido)
+
+    firma=document.getElementById ("firma").value
+    document.getElementById("resultado28").innerHTML="<h3> firma: "+firma+"</h3>"
+    console.log(firma)
+
+    firma1=document.getElementById ("firma1").value
+    document.getElementById("resultado29").innerHTML="<h3> firma1: "+firma1+"</h3>"
+    console.log(firma1)
+
+    firma2=document.getElementById ("firma2").value
+    document.getElementById("resultado30").innerHTML="<h3> firma2: "+firma2+"</h3>"
+    console.log(firma2)
+
+    fecha=document.getElementById ("fecha").value
+    document.getElementById("resultado31").innerHTML="<h3> fecha: "+fecha+"</h3>"
+    console.log(fecha)
+
+    fecha1=document.getElementById ("fecha1").value
+    document.getElementById("resultado32").innerHTML="<h3> fecha1: "+fecha1+"</h3>"
+    console.log(fecha1)
+
+    fecha2=document.getElementById ("fecha2").value
+    document.getElementById("resultado33").innerHTML="<h3> fecha2: "+fecha2+"</h3>"
+    console.log(fecha2)
+
+    version11=document.getElementById ("version11").value
+    document.getElementById("resultado34").innerHTML="<h3> verciones anteriores: "+version11+"</h3>"
+    console.log(version11)
+
+    version10=document.getElementById ("version10").value
+    document.getElementById("resultado35").innerHTML="<h3> vercion actual "+version10+"</h3>"
+    console.log(version10)
+}
